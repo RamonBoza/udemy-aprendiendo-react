ddede7e2aecbff81d3f99fea906bffd55f11850a
Ramón Boza <Ramon.Boza@barltm6498.midasplayer.local>
2019-06-06 15:02:14 +0200

    Section 6 - Adding a form

--- udemy-curso/src/App.js
+++ udemy-curso/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Forms from './sections/forms'
 
 
 class App extends Component {
@@ -23,13 +24,7 @@ class App extends Component {
 
     return (
         <div className="App">
-          <h4>Eventos</h4>
-          <button onClick={this.handleClick}>Hi there!</button>
-          <div
-            onMouseMove={this.handleMouseMove}
-            style={{ border: '1px solid #000', marginTop: 10, padding: 10 }}>
-            <p>{this.state.mouseX}, {this.state.mouseY}</p>
-          </div>
+          <Forms />
         </div>
     )
   }
