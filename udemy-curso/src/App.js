3ac3f6c6156d76b8ac91dc98dfa2d82730e12cdc
Ramón Boza <Ramon.Boza@barltm6498.midasplayer.local>
2019-06-06 11:43:50 +0200

    Displaying list of elements

--- udemy-curso/src/App.js
+++ udemy-curso/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import ConditionalSection from './sections/conditional'
 
-function App() {
-  return (
-    <div className="App">
-      <ConditionalSection />
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+
+    const numbers = [1, 1, 3, 4, 5]
+
+    return (
+        <div className="App">
+          {numbers.map((number, index) => {
+            return <p key={index}>Soy el numero {number}</p>
+          })}
+        </div>
+    )
+  }
 }
 
 export default App;
