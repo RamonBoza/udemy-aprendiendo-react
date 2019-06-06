daaf287d9fe15c024e97243e07f75657f5c62a18
Ramón Boza <Ramon.Boza@barltm6498.midasplayer.local>
2019-06-06 15:13:49 +0200

    Changing to controlled form instead of uncontrolled

--- udemy-curso/src/sections/forms.js
+++ udemy-curso/src/sections/forms.js
@@ -2,26 +2,42 @@ import React, { Component } from 'react'
 
 export default class Forms extends Component {
 
+    constructor() {
+        super()
+        this.state = {
+            inputName: '',
+            inputTwitter: '@',
+            inputTerms: true
+        }
+    }
 
-    handleClick = (e) => {
+    handleSubmit = (e) => {
         e.preventDefault()
         const name = this.inputName.value
         const email = document.getElementById('twitter').value
         console.log({name, email})
     }
 
+    handleChange = (e) => {
+        console.log('handleChange')
+        console.log(e.target.checked)
+        this.setState({ inputTerms: e.target.checked })
+    }
+
     render() {
         return (
             <div>
                 <h4>Formularios</h4>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <p>
                         <label htmlFor='name'>Nombre: </label>
                         <input
                             id='name'
                             name='userName'
+                            onChange={e => this.setState({inputName: e.target.value })}
                             placeholder='Introduce el nombre'
                             ref={inputElement => this.inputName = inputElement }
+                            value={this.state.inputName}
                         />
                     </p>
                     <p>
@@ -29,9 +45,25 @@ export default class Forms extends Component {
                         <input
                             id='twitter'
                             name='twitterAccount'
-                            placeholder='Introduce tu Twitter' />
+                            onChange={e => this.setState({ inputTwitter: e.target.value })}
+                            placeholder='Introduce tu Twitter'
+                            value={this.state.inputTwitter}
+                        />
                     </p>
-                    <button onClick={this.handleClick}>Enviar</button>
+
+                    <p>
+                        <label>
+                            <input
+                                checked={this.state.inputTerms}
+                                onChange={this.handleChange}
+                                type='checkbox' />
+                                Accepted terms
+                        </label>
+                    </p>
+
+
+
+                    <button >Enviar</button>
                 </form>
             </div>
         )
