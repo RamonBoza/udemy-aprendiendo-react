2fa0c2078763043c3392fcd211cb5277789308c3
Ramón Boza <Ramon.Boza@barltm6498.midasplayer.local>
2019-06-06 12:35:15 +0200

    Section 6 - on click event

--- udemy-curso/src/App.js
+++ udemy-curso/src/App.js
@@ -5,13 +5,12 @@ class App extends Component {
 
   render() {
 
-    const numbers = [1, 1, 3, 4, 5]
+
 
     return (
         <div className="App">
-          {numbers.map((number, index) => {
-            return <p key={index}>Soy el numero {number}</p>
-          })}
+          <h4>Eventos</h4>
+          <button onClick={() => alert('Hi there!')}>Hi there!</button>
         </div>
     )
   }
