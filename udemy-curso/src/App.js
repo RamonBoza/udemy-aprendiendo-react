8a03d620f43d774baea1a4a06d03b67aa7e048e9
Ramón Boza <Ramon.Boza@barltm6498.midasplayer.local>
2019-06-06 12:48:05 +0200

    Binding mouse events to class

--- udemy-curso/src/App.js
+++ udemy-curso/src/App.js
@@ -3,14 +3,33 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
+  constructor() {
+    super()
+    this.state = { mouseX: 0, mouseY: 0 }
+    // this.handleMouseMove = this.handleMouseMove.bind(this) // replacing by arrow function
+  }
+
+  handleMouseMove = (e) => {
+    const { clientX, clientY } = e
+    this.setState({ mouseX: clientX, mouseY: clientY })
+  }
+
+  handleClick (e) {
+    console.log(e)
+    alert('Hi here!')
+  }
+
   render() {
-
-
 
     return (
         <div className="App">
           <h4>Eventos</h4>
-          <button onClick={() => alert('Hi there!')}>Hi there!</button>
+          <button onClick={this.handleClick}>Hi there!</button>
+          <div
+            onMouseMove={this.handleMouseMove}
+            style={{ border: '1px solid #000', marginTop: 10, padding: 10 }}>
+            <p>{this.state.mouseX}, {this.state.mouseY}</p>
+          </div>
         </div>
     )
   }
