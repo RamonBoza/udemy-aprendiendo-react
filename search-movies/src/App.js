8175ae61db7e13d8c30060ec6a1492ef1a7e8cba
Ramón Boza <Ramon.Boza@barltm6498.midasplayer.local>
2019-06-13 15:07:16 +0200

    Adding basic routing to different pages

--- search-movies/src/App.js
+++ search-movies/src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 import { Title } from './components/Title'
 import { SearchForm } from "./components/SearchForm";
+
 import './App.css';
 import 'bulma/css/bulma.css'
 import {MoviesList} from "./components/MoviesList";
+import {Detail} from "./pages/Detail";
 
 class App extends Component {
   state = { usedSearch: false, results: []}
@@ -20,6 +22,14 @@ class App extends Component {
   }
 
   render() {
+
+    const url = new URL(document.location)
+    const hasID = url.searchParams.has('id')
+
+    if(hasID){
+      return <Detail id={url.searchParams.get('id')}/>
+    }
+
     return (
         <div className="App">
           <Title>Search Movies</Title>
