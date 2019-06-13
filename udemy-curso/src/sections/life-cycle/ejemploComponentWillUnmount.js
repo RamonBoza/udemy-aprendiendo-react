511af885cc3d073cf0e48bda65709665e0290746
Ramón Boza <Ramon.Boza@barltm6498.midasplayer.local>
2019-06-13 11:16:18 +0200

    Section 8 - component will unmount finished

--- udemy-curso/src/sections/life-cycle/ejemploComponentWillUnmount.js
+++ udemy-curso/src/sections/life-cycle/ejemploComponentWillUnmount.js
@@ -1,10 +1,63 @@
 import React, { Component } from 'react';
 
+
+class ComponenteADesmontar extends Component {
+    state = { windowWidth: 0 }
+
+    _updateStateWithWindowWidth = () => {
+        console.log('_updateStateWithWindowWidth')
+        this.setState({ windowWidth: document.body.clientWidth })
+    }
+
+    componentDidMount () {
+        this._updateStateWithWindowWidth()
+        window.addEventListener(
+            'resize',
+            this._updateStateWithWindowWidth
+        )
+    }
+
+    componentWillUnmount () {
+        console.log('componentWillUnmount')
+        window.removeEventListener(
+            'resize',
+            this._updateStateWithWindowWidth
+        )
+    }
+
+    render () {
+        return (
+            <div>
+                <p>Ancho de la ventana: {this.state.windowWidth}</p>
+            </div>
+        )
+    }
+}
+
+
+
 class EjemploComponentWillUnmount extends Component {
 
+    state = { mostrarComponent: true }
+
     render() {
+
+        if (this.state.mostrarComponent) {
+            return (
+                <div>
+                    <h4>Ciclo de desmontaje: componentWillUnmount</h4>
+                    <ComponenteADesmontar/>
+                    <button onClick={() => this.setState({mostrarComponent: false})}>
+                        Desmontar component
+                    </button>
+                </div>
+            )
+        }
+
         return (
-            <div></div>
+            <button onClick={() => this.setState({mostrarComponent: true})}>
+                Montar component
+            </button>
         )
     }
 }
